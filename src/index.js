d30a6da4ea7aa0069fcc504dbb443b30a91ea750
Francisca <egofrancisca@gmail.com>
2026-07-14 15:55:16 +0100

    Resolved all merge conflicts across project files

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
-=======
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // ✅ added
->>>>>>> eecf6611ba0f362931e516c3f4743e21ade3df8b
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,9 +16,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-<<<<<<< HEAD
-=======
 
 // ✅ register the service worker for offline caching
 serviceWorkerRegistration.register();
->>>>>>> eecf6611ba0f362931e516c3f4743e21ade3df8b
